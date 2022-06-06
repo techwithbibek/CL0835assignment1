f2735f6aca270c8418ea6c090eabfe6d600ae2bd
bibek017 <shreesbibek@gmail.com>
2022-06-05 23:06:06 -0400

    my terraform code

---
+++ terraform_code/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Bibek"
+    "App"   = "Web"
+    "Project" = "Assignment1"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "assignment"
+}
